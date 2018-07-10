20e0237d8c4830cab3e7d9b0cfe0b6299dafa0ce
baibing <349731984@qq.com>
2018-07-10 14:56:48 +0800

    添加了两个ts文件

---
+++ vuecli/src/components/TabRouterSlipe.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class='app_name'>
+        {{m}}
+        <div class="b"></div>
+    </div>
+</template>
+<script>
+
+export default {
+name :'TabRouterSlipe',
+    data () {
+    	return {
+    		m :'fuck u'
+    	}
+    },
+    created () {
+       console.log(this.out('you'))
+    },
+    components: {
+
+    },
+    methods: {
+        out:function(person){
+            return "Hello, " + person;       
+        }   
+    }
+}
+</script>
+<style scoped>
+.app_name {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.app_name div.b{
+    width: 400px;
+    height: 200px;  
+    background: red;
+    margin: 10px auto;
+}
+</style>
